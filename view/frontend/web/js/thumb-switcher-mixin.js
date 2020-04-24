b62ac074d2d2d7454977e04c16321d86c447c367
Erik Hansen <erik@krakencommerce.com>
2020-04-24 13:24:17 -0500

    Force user to click on video thumbnail, even if "Swap trigger: hover" is configured

--- view/frontend/web/js/thumb-switcher-mixin.js
+++ view/frontend/web/js/thumb-switcher-mixin.js
@@ -15,6 +15,33 @@ define([
       jQuery['mage']['magicToolboxThumbSwitcher'],
       {
         /**
+         * Bind handler to elements
+         * @protected
+         */
+        _bind: function () {
+
+          var switchMethod = this.options.switchMethod,
+            thumbs = this.options.thumbs;
+
+          if (this.options.isMagicZoom) {
+            switchMethod = (switchMethod == 'click' ? 'btnclick' : switchMethod);
+          }
+
+          var switchThumbFn = $.proxy(this._switchThumb, this);
+          for (var i = 0; i < thumbs.length; i++) {
+            // BEGIN EDIT - Force user to click on video thumbnail, even if "Swap trigger: hover" is configured
+            if ($(thumbs[i]).hasClass('video-selector')) {
+              var videoThumbnail = $(thumbs[i]);
+              videoThumbnail.attr('title', "Click to watch video");
+              $mjs(thumbs[i]).jAddEvent('click'+' tap', switchThumbFn, 1);
+              return;
+            }
+            // END EDIT
+            $mjs(thumbs[i]).jAddEvent(switchMethod+' tap', switchThumbFn, 1);
+          }
+        },
+
+        /**
          * Override the MagicScroll/MagicZoomPlus logic so that if a video thumbnail is clicked, user is scrolled down
          * to video section
          *
